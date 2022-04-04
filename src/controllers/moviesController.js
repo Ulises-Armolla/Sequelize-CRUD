76c16f809857e667321a340dbb6d261b4be71c8c
Ulises Armolla <uliarmolla@gmail.com>
2022-04-03 20:34:04 -0500

    Subo sequelize crud y consultas basicas preparadas

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,118 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+const moviesController = {
+
+    list: (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList', {movies})
+            })
+    },
+    detail: (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail', {movie});
+            });
+    },
+    new: (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    recomended: (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, 
+    
+    //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+    
+    add: function (req, res) {
+        res.render('moviesAdd');  
+    },
+
+    create: function (req, res) {
+        const { title, rating, release_date, length, awards } = req.body;
+
+        db.Movie.create({
+            title,
+            rating,
+            release_date,
+            length, 
+            awards
+        })
+        .then((movie) => {
+            res.redirect('/movies');
+        })
+        .catch((error) => {
+            res.send(error);
+        });
+    },
+
+    edit: function(req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+            res.render('moviesEdit', {
+                movie
+            });
+        });
+    },
+
+    update: function (req,res) {
+        const { title, rating, release_date, length, awards } = req.body;
+        db.Movie.update({
+            title, 
+            rating, 
+            release_date, 
+            length, 
+            awards
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((result) => {
+            res.redirect('/movies');
+        })
+        .catch((error) => {
+            res.send(error);
+        })
+    },
+
+    delete: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+            res.render('moviesDelete', {
+                movie
+            });
+        })
+    },
+
+    destroy: function (req, res) {
+        db.Movie.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((result) => {
+            res.redirect('/movies');
+        })
+    }
+}
+
+module.exports = moviesController;
